b6b6e75479611f86fbee13d742b5a2e06c8118f9
Roderic Page <rdmpage@gmail.com>
2018-07-19 18:16:18 +0100

    Add JSON-LD and some triple store code

---
+++ config.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file config.inc.php
+ *
+ * Global configuration variables (may be added to by other modules).
+ *
+ */
+
+global $config;
+
+// Date timezone
+date_default_timezone_set('UTC');
+
+$site = 'local';
+$site = 'heroku';
+
+switch ($site)
+{
+	case 'heroku':
+		// Server-------------------------------------------------------------------------
+		$config['web_server']	= 'https://ozymandias-demo.herokuapp.com'; 
+		$config['site_name']	= 'Ozymandias';
+
+		// Files--------------------------------------------------------------------------
+		$config['web_dir']		= dirname(__FILE__);
+		$config['web_root']		= '/';		
+		break;
+
+	case 'local':
+	default:
+		// Server-------------------------------------------------------------------------
+		$config['web_server']	= 'http://localhost'; 
+		$config['site_name']	= 'Ozymandias';
+
+		// Files--------------------------------------------------------------------------
+		$config['web_dir']		= dirname(__FILE__);
+		$config['web_root']		= '/~rpage/ozymandias-demo/';
+}
+
+$config['sparql_endpoint'] 	= '';
+$config['triplestore'] 		= 'blazegraph';
+
+if ($config['triplestore'] == 'blazegraph')
+{
+	$config['blazegraph-url'] 	= 'http://130.209.46.63';	
+	$config['sparql_endpoint']	= $config['blazegraph-url'] . '/blazegraph/sparql'; 
+}
+
+?>
---
+++ sparql.php
@@ -0,0 +1,245 @@
+<?php
+
+error_reporting(E_ALL);
+
+require_once('vendor/digitalbazaar/json-ld/jsonld.php');
+
+// SPARQL API wrapper
+
+
+//----------------------------------------------------------------------------------------
+// Upload a file of triples
+// $triples_filename is the full path to a file of triples
+// $graph_key_name for fuseki is 'graph', for blazegraph is 'context-uri'
+function upload_from_file($sparql_endpoint, $triples_filename, $graph_key_name = 'context-uri', $graph_uri = '')
+{
+	$url = $sparql_endpoint;
+	
+	if ($graph_uri == '')
+	{
+	}
+	else
+	{
+		$url .= '?' . $graph_key_name . '=' . $graph_uri;
+	}
+	
+	// text/x-nquads is US-ASCII WTF!?
+	//$command = "curl $url -H 'Content-Type: text/x-nquads' --data-binary '@$triples_filename'";
+
+	// text/rdf+n3 is compatible with NT and is UTF-8
+	// see https://wiki.blazegraph.com/wiki/index.php/REST_API#RDF_data
+	$command = "curl $url -H 'Content-Type: text/rdf+n3' --data-binary '@$triples_filename'";
+
+	echo $command . "\n";
+	
+	$lastline = system($command, $retval);
+	
+	//echo "   Last line: $lastline\n";
+	//echo "Return value: $retval\n";	
+	
+	if (preg_match('/data modified="0"/', $lastline)) 
+	{
+		echo "\nError: no data added\n";
+		exit();
+	}
+}
+
+
+//----------------------------------------------------------------------------------------
+// DESCRIBE a resource, by default return as JSON-LD
+// Fuseki and Blazegraph both recognise application/ld+json but for quads
+// Fuseki uses application/n-quads whereas Blazegraph uses text/x-nquads
+function sparql_describe($sparql_endpoint, $uri, $format='application/ld+json')
+{
+	$url = $sparql_endpoint;
+	
+	// Query is string
+	$data = 'query=' . urlencode('DESCRIBE <' . $uri . '>');
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: " . $format));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+	$response = curl_exec($ch);
+	if($response == FALSE) 
+	{
+		$errorText = curl_error($ch);
+		curl_close($ch);
+		die($errorText);
+	}
+	
+	$info = curl_getinfo($ch);
+	$http_code = $info['http_code'];
+	
+	if ($http_code != 200)
+	{
+		echo $response;	
+		die ("Triple store returned $http_code\n");
+	}
+	
+	curl_close($ch);
+	
+	// Fuseki returns nicely formatted JSON-LD, Blazegraph returns array of horrible JSON-LD
+	// as first element of an array
+	
+	$obj = json_decode($response);
+	if (is_array($obj))
+	{
+		$doc = $obj[0];
+		
+		$context = (object)array(
+			'@vocab' => 'http://schema.org/'
+		);
+	
+		$compacted = jsonld_compact($doc, $context);
+		
+		$response = json_encode($compacted, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);		
+	}
+	
+
+	return $response;
+}
+
+//----------------------------------------------------------------------------------------
+// CONSTRUCT a resource, by default return as JSON-LD
+function sparql_construct($sparql_endpoint, $uri, $format='application/ld+json')
+{
+	$url = $sparql_endpoint;
+	
+	// Query is string
+	$query = 'CONSTRUCT {
+   ?thing ?p ?o .
+}
+WHERE {
+  VALUES ?thing { <' . $uri . '> }
+   ?thing ?p ?o .
+}';	
+
+	$data = 'query=' . urlencode($query);
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: " . $format));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+	$response = curl_exec($ch);
+	if($response == FALSE) 
+	{
+		$errorText = curl_error($ch);
+		curl_close($ch);
+		die($errorText);
+	}
+	
+	$info = curl_getinfo($ch);
+	$http_code = $info['http_code'];
+	
+	if ($http_code != 200)
+	{
+		echo $response;	
+		die ("Triple store returned $http_code\n");
+	}
+	
+	curl_close($ch);
+	
+	// Fuseki returns nicely formatted JSON-LD, Blazegraph returns array of horrible JSON-LD
+	// as first element of an array
+	
+	$obj = json_decode($response);
+	if (is_array($obj))
+	{
+		$doc = $obj[0];
+		
+		$context = (object)array(
+			'@vocab' => 'http://schema.org/'
+		);
+	
+		$compacted = jsonld_compact($doc, $context);
+		
+		$response = json_encode($compacted, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);		
+	}
+	
+
+	return $response;
+}
+
+//----------------------------------------------------------------------------------------
+// QUERY, by default return as JSON
+function sparql_query($sparql_endpoint, $query, $format='application/json')
+{
+	$url = $sparql_endpoint . '?query=' . urlencode($query);
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: " . $format));
+
+	$response = curl_exec($ch);
+	if($response == FALSE) 
+	{
+		$errorText = curl_error($ch);
+		curl_close($ch);
+		die($errorText);
+	}
+	
+	$info = curl_getinfo($ch);
+	$http_code = $info['http_code'];
+	
+	if ($http_code != 200)
+	{
+		echo $response;	
+		die ("Triple store returned $http_code\n");
+	}
+	
+	curl_close($ch);
+
+	return $response;
+}
+
+
+// test
+
+if (0)
+{
+	
+	upload_from_file(
+		'http://localhost:32774/blazegraph/sparql',
+		'rdf/309.nq',
+		'context-uri',
+		'http://www.ipni.org'
+		);
+
+
+}
+
+if (0)
+{
+	$response = sparql_describe(
+	'http://localhost:32774/blazegraph/sparql',
+	'urn:lsid:ipni.org:names:309362-1'
+	);
+	
+	echo $response;
+	
+	
+}
+
+if (0)
+{
+	$response = sparql_query(
+	'http://localhost:32779/blazegraph/sparql',
+	'SELECT * WHERE { ?s ?p ?o . } LIMIT 5'
+	);
+	
+	echo $response;
+		
+}
+
+?>
